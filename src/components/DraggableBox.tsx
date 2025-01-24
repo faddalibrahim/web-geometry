08ff112434310338045a52bef378206909a2987e
faddalibrahim <faddalibrahim@gmail.com>
2025-01-24 01:51:07 +0000

    add markers for bottom and right

--- src/components/DraggableBox.tsx
+++ src/components/DraggableBox.tsx
@@ -14,12 +14,24 @@ const DraggableBox: React.FC = () => {
   const startingXRef = useRef(0);
   const startingYRef = useRef(0);
 
+  const rMarkerRef = useRef<HTMLDivElement>(null);
+  const bMarkerRef = useRef<HTMLDivElement>(null);
+
   // Add state for x and y values
   const [xValue, setXValue] = useState(0);
   const [yValue, setYValue] = useState(0);
 
+  const [rValue, setRValue] = useState(0);
+  const [bValue, setBValue] = useState(0);
+
   const handleMouseMove = useCallback((event: MouseEvent) => {
-    if (boxRef.current && xMarkerRef.current && yMarkerRef.current) {
+    if (
+      boxRef.current &&
+      xMarkerRef.current &&
+      yMarkerRef.current &&
+      rMarkerRef.current &&
+      bMarkerRef.current
+    ) {
       const distX = event.clientX - startingXRef.current;
       const distY = event.clientY - startingYRef.current;
 
@@ -35,8 +47,17 @@ const DraggableBox: React.FC = () => {
       yMarkerRef.current.style.height = `${newTop}px`;
       yMarkerRef.current.style.left = `${newLeft}px`;
 
+      rMarkerRef.current.style.width = `${newLeft + 100}px`;
+      rMarkerRef.current.style.top = `${newTop + 100}px`;
+
+      bMarkerRef.current.style.left = `${newLeft + 100}px`;
+      bMarkerRef.current.style.height = `${newTop + 100}px`;
+
       setXValue(newLeft);
       setYValue(newTop);
+
+      setRValue(newLeft + 100);
+      setBValue(newTop + 100);
     }
   }, []);
 
@@ -67,9 +88,12 @@ const DraggableBox: React.FC = () => {
     const box = boxRef.current;
     const xMarker = xMarkerRef.current;
     const yMarker = yMarkerRef.current;
+    const rMarker = rMarkerRef.current;
+    const bMarker = bMarkerRef.current;
 
-    if (box && xMarker && yMarker) {
-      const { top, left } = box.getBoundingClientRect();
+    if (box && xMarker && yMarker && rMarker && bMarker) {
+      const { top, left, right, height, bottom, width } =
+        box.getBoundingClientRect();
 
       xMarker.style.top = `${top}px`;
       xMarker.style.width = `${left}px`;
@@ -77,8 +101,17 @@ const DraggableBox: React.FC = () => {
       yMarker.style.left = `${left}px`;
       yMarker.style.height = `${top}px`;
 
+      rMarker.style.width = `${right}px`;
+      rMarker.style.top = `${top + height}px`;
+
+      bMarker.style.height = `${bottom}px`;
+      bMarker.style.left = `${left + width}px`;
+
       setXValue(left);
       setYValue(top);
+
+      setRValue(right);
+      setBValue(bottom);
 
       box.addEventListener("mousedown", handleMouseDown);
     }
@@ -108,6 +141,19 @@ const DraggableBox: React.FC = () => {
         label="y / top"
         orientation={VERTICAL}
         value={yValue}
+      />
+
+      <Marker
+        mRef={rMarkerRef}
+        label="right"
+        orientation={HORIZONTAL}
+        value={rValue}
+      />
+      <Marker
+        mRef={bMarkerRef}
+        label="bottom"
+        orientation={VERTICAL}
+        value={bValue}
       />
     </>
   );
